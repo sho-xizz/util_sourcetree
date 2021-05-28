582e2bcbabf74b10e9f2944b6dde92c42353000b
sho-xizz <80442573+sho-xizz@users.noreply.github.com>
2021-05-29 00:35:11 +0900

    Update export_diff.sh
    add SHA into export directory name.

--- export_diff.sh
+++ export_diff.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-today=`date +"%y%m%d_%H%M%S"`
-diff_dir='./__diff_archive/'
+# ########
+# CustomAction for Sourcetree to export the diff-files.
+# ########
+# 
+TODAY=`date +"%y%m%d_%H%M%S"`
+SHA1="${1:0:7}"
+SHA2="${2:0:7}"
+DIFF_DIR='./__diff_archive/'
+
 if [ "$2" = "" ]; then
     echo 'Select any two commit to generate diff.'
 else
-    git checkout-index -f --prefix=$diff_dir$today/ $1 `git diff -M100% --name-only $2..$1`
-    open $diff_dir
+    git checkout-index -f --prefix="${DIFF_DIR}${TODAY}__${SHA1}-${SHA2}/" $1 `git diff -M100% --name-only $2..$1`
+    open $DIFF_DIR
 fi
