e6207f5bd1b1d139f8748e8cc2d55d96e75bc5f7
sho-xizz <80442573+sho-xizz@users.noreply.github.com>
2021-05-29 01:28:20 +0900

    Update log_diff-files.sh
    add SHA to diff-list name.

--- log_diff-files.sh
+++ log_diff-files.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-today=`date +"%y%m%d_%H%M%S"`
-log_dir='./__diff_archive/__log/'
+# ########
+# CustomAction for Sourcetree to export the diff-files.
+# ########
+# 
+SHA1="${1:0:7}"
+SHA2="${2:0:7}"
+LOG_DIR='./__diff_archive/__log/'
 if [ "$2" = "" ]; then
     echo 'Select any two commit to generate diff.'
 else
-    git diff -M100% --name-only --diff-filter=AMCRD $2..$1 >> ${log_dir}list_tag.txt
-    open $log_dir
+    git diff -M100% --name-only --diff-filter=AMCRD $2..$1 >> "${LOG_DIR}diff-list__${SHA1}-${SHA2}.txt"
+    open $LOG_DIR
 fi
